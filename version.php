bc7db9b7b9647145b006ea8b1393524e90edd092
Daniel Thies <dethies@gmail.com>
2020-07-15 22:04:06 -0500

    Release version 1.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qformat_h5p';
-$plugin->release = '1.0 Beta 6';
-$plugin->version = 2020060905;
+$plugin->release = '1.0';
+$plugin->version = 2020071500;
 $plugin->requires = 2018051700;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;

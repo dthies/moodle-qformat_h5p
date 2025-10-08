8885163fe660678328c8c3b6b013c628bba9be52
Daniel Thies <dthies@gmail.com>
2025-10-07 21:30:04 -0500

    Update version to support 5.1

--- version.php
+++ version.php
@@ -26,6 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qformat_h5p';
 $plugin->release = '1.0.8';
-$plugin->version = 2020071512;
+$plugin->version = 2020071513;
 $plugin->requires = 2018051700;
+$plugin->supported = [35, 501];
 $plugin->maturity = MATURITY_STABLE;
